3f9aa2274e2ff483e3d7c2fe57d3a00a6b90d5fc
Ryusei Yanagita <ryanagit@c4r1s11.42tokyo.jp>
2024-11-28 20:15:05 +0900

    hello

---
+++ cgi/CgiHandler.hpp
@@ -0,0 +1,31 @@
+#ifndef CGIHANDLER_HPP
+#define CGIHANDLER_HPP
+
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <cstring>
+#include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include "../request/HTTPRequest.hpp"
+#include "CgiResponse.hpp"
+
+class CgiHandler
+{
+private:
+	std::map<std::string, std::string> env_vars_;
+	std::string request_body_;
+	HTTPRequest &request_;
+
+public:
+	CgiHandler(HTTPRequest &request);
+	void setRequestBody(const std::string &body);
+	std::string ExecuteCGI();
+};
+
+#endif
---
+++ cgi/cgitest_main.cpp
@@ -0,0 +1,15 @@
+#include "CgiHandler.hpp"
+// test用main関数 環境変数を設定してCGIを実行しレスポンスを表示
+
+int main()
+{
+	HTTPRequest request;
+
+	CgiHandler cgi_handler(request);
+
+	std::string response = cgi_handler.ExecuteCGI();
+
+	std::cout << "CGI Response:\n\n" << response << std::endl;
+
+	return 0;
+}
